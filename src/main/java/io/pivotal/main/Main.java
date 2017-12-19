b90687575d547e1299f9a67b2b78e2eea38ba6c6
Danil Kornishev <danil.kornishev@gmail.com>
2017-12-19 11:07:54 -0500

    Simple Spring Boot app

---
+++ src/main/java/io/pivotal/main/Main.java
@@ -0,0 +1,10 @@
+package io.pivotal.main;
+
+/**
+ * @author Danil Kornishev (danil.kornishev@mastercard.com)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
